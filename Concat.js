9d213328b30fe764e1f97884b85bd03e318ad04a
sujeetku123 <102528893+sujeetku123@users.noreply.github.com>
2022-05-20 13:51:08 +0530

    Create Concat.js

---
+++ Concat.js
@@ -0,0 +1,7 @@
+function Concat() {
+    const arr1 = ["a", "b", "c"];
+    const arr2 = ["1", "2", "3", "4"];
+    const children = arr1.concat(arr2);
+    console.log("concatinated")
+    document.write("Concatinated array is: " + children);
+}
